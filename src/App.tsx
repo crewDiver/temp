cdec5d073d658ef58a6acc42c964ed34e97bd2f4
Alexandr Pirogov <otbitij@mail.ru>
2023-04-02 19:16:38 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Rating} from "./compnents/Rating/Rating";
+import Accordion from "./compnents/Accordion/Accordion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("App rendering")
+    return (
+        <div>
+            <Rating value = {3}/>
+            <Accordion/>
+            <Rating value = {4}/>
+        </div>
+    );
 }
 
 export default App;
